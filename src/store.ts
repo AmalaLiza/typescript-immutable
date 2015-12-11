fdc5e5869f9ea6032fa16357899fe70072775d45
AmalaLiza <amala.james@razorthink.net>
2015-12-11 13:02:43 +0530

    Configuration

---
+++ src/store.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/redux/redux.d.ts" />
+
+import { compose, createStore, applyMiddleware } from 'redux';
+import { rootReducer } from './reducers.ts';
+
+
+const finalCreateStore = compose()(createStore);
+
+export default function configureStore () {
+    return finalCreateStore(rootReducer);
+}
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:5000',
+        'webpack/hot/only-dev-server',
+        './src/index.tsx'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                exclude: /node_modules/,
+                loader: 'react-hot'
+            },
+            {
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            }
+        ],
+        include: path.join(__dirname, 'src')
+    }
+};
